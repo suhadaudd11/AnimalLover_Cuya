89fa84eaecb01fd71aea1d7babef2cc757bd668d
suhadaudd11 <nasuha@um.edu.my>
2024-11-18 16:10:00 +0800

    Change android sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.animallover_cuya"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.animallover_cuya"
